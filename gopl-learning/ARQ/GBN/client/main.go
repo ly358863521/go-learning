5533a4de783d19f26e5a9e39086e0d87ccd51250
liyan <358863520@qq.com>
2019-10-30 23:03:06 +0800

    ARQ

--- gopl-learning/ARQ/GBN/client/main.go
+++ gopl-learning/ARQ/GBN/client/main.go
@@ -6,11 +6,21 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"time"
 )
 
-var host = flag.String("host", "localhost", "host")
-var port = flag.String("port", "37", "port")
+var (
+	host               = flag.String("host", "localhost", "host")
+	port               = flag.String("port", "8080", "port")
+	wind_size          = 10
+	seq_size           = 10
+	ack         []bool = make([]bool, seq_size) //收到ack情况
+	curSeq      int                             //当前数据包序号
+	curAck      int                             //当前等待确认的ack
+	totalSeq    int                             //收到的包的总数
+	totalPacket int                             //需要发送的包总数
+)
 
 //go run timeclient.go -host time.nist.gov
 func main() {
@@ -25,8 +35,14 @@ func main() {
 		fmt.Println("Can't dial: ", err)
 		os.Exit(1)
 	}
+	fmt.Println("connected!")
 	defer conn.Close()
-	_, err = conn.Write([]byte(""))
+	// bs := make([]byte, 10)
+	// binary.BigEndian.PutUint64(bs, 123)
+	// fmt.Println(bs)
+	bs := []byte(strconv.Itoa(123))
+	fmt.Println(bs)
+	_, err = conn.Write(bs)
 	if err != nil {
 		fmt.Println("failed:", err)
 		os.Exit(1)
--- gopl-learning/ARQ/GBN/server/main.go
+++ gopl-learning/ARQ/GBN/server/main.go
@@ -6,11 +6,21 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"time"
 )
 
-var host = flag.String("host", "", "host")
-var port = flag.String("port", "37", "port")
+var (
+	host               = flag.String("host", "", "host")
+	port               = flag.String("port", "8080", "port")
+	wind_size          = 10
+	seq_size           = 10
+	endrecv            = -1
+	ack         []bool = make([]bool, seq_size) //收到ack情况
+	curAck      int                             //当前等待确认的ack
+	totalSeq    int                             //收到的包的总数
+	totalPacket int                             //需要发送的包总数
+)
 
 func main() {
 	flag.Parse()
@@ -29,16 +39,29 @@ func main() {
 		handleClient(conn)
 	}
 }
+
 func handleClient(conn *net.UDPConn) {
-	data := make([]byte, 1024)
-	n, remoteAddr, err := conn.ReadFromUDP(data)
-	if err != nil {
-		fmt.Println("failed to read UDP msg because of ", err.Error())
-		return
+	for {
+		data := make([]byte, 100)
+		n, remoteAddr, err := conn.ReadFromUDP(data)
+		if err != nil {
+			fmt.Println("failed to read UDP msg because of ", err.Error())
+			return
+		}
+		// fmt.Println(data)
+		// seq := binary.BigEndian.Uint64(data)
+		seq, err := strconv.Atoi(string(data[:n]))
+		fmt.Println(seq)
+		daytime := time.Now().Unix()
+		fmt.Println(n, remoteAddr)
+		b := make([]byte, 4)
+		binary.BigEndian.PutUint32(b, uint32(daytime))
+		conn.WriteToUDP(b, remoteAddr)
+
 	}
-	daytime := time.Now().Unix()
-	fmt.Println(n, remoteAddr)
-	b := make([]byte, 4)
-	binary.BigEndian.PutUint32(b, uint32(daytime))
-	conn.WriteToUDP(b, remoteAddr)
+	// daytime := time.Now().Unix()
+	// fmt.Println(n, remoteAddr)
+	// b := make([]byte, 4)
+	// binary.BigEndian.PutUint32(b, uint32(daytime))
+	// conn.WriteToUDP(b, remoteAddr)
 }
