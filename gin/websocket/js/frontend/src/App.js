a6665b0ac5c6ebd4f5cad44f8328a1f623b90490
liyan <358863520@qq.com>
2019-11-21 21:32:09 +0800

    [react]

--- gin/websocket/js/frontend/src/App.js
+++ gin/websocket/js/frontend/src/App.js
@@ -2,9 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { throwStatement } from '@babel/types';
+import { Table } from 'antd';
+
+const columns = [
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    render: () => <a>Delete</a>,
+  },
+];
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+  },
+];
+
 
 class Clock extends React.Component {
-  
   
   state = {
     b: 12,
@@ -29,8 +66,16 @@ class Clock extends React.Component {
       <div>
         <h1>Hello, world!</h1>
         <h2>It is clock.  {this.state.b}</h2>
-    <p>{this.state.data}</p>
+      <p>{this.state.data}</p>
       </div>
+      
+    //    <Table
+    //    columns={columns}
+    //    expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
+    //    dataSource={data}
+    //  />
+
+      
     );
   }
 }
