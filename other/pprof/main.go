edb8ca9f611487a904b24fbd81be965133f02e1e
liyan <358863520@qq.com>
2019-12-15 22:18:04 +0800

    [pprof]

---
+++ other/pprof/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof" // 为什么用_ , 在讲解http包时有解释。
+)
+
+func myfunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hi")
+}
+
+func main() {
+	http.HandleFunc("/", myfunc)
+	http.ListenAndServe(":8080", nil)
+}
+
+// 访问http://localhost:8080/debug/pprof/
